d4fabbfb4fed232447db06577556a908b56cf5e3
Oriol Tristany <otristany@alphanet.cat>
2020-09-24 10:44:55 +0200

    makes it workable

--- src/main/java/com/orioltristany/templates/springbootcommandlinetemplate/SpringbootCommandlineTemplateApplication.java
+++ src/main/java/com/orioltristany/templates/springbootcommandlinetemplate/SpringbootCommandlineTemplateApplication.java
@@ -1,13 +1,21 @@
 package com.orioltristany.templates.springbootcommandlinetemplate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootCommandlineTemplateApplication {
+public class SpringbootCommandlineTemplateApplication implements CommandLineRunner {
+    private static Logger logger = LoggerFactory.getLogger(SpringbootCommandlineTemplateApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootCommandlineTemplateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCommandlineTemplateApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("hello world");
+    }
 }
