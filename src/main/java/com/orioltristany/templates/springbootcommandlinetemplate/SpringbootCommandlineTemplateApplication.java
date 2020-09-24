0ba965b99cdf496c9a485df24f4ead58523a78f4
Oriol Tristany <otristany@alphanet.cat>
2020-09-24 10:24:31 +0200

    adds first commti

---
+++ src/main/java/com/orioltristany/templates/springbootcommandlinetemplate/SpringbootCommandlineTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.orioltristany.templates.springbootcommandlinetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCommandlineTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCommandlineTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orioltristany/templates/springbootcommandlinetemplate/SpringbootCommandlineTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orioltristany.templates.springbootcommandlinetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommandlineTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
